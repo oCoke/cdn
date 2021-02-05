961b99bc00d7ed1eb1fe0f6db62b661f3c13badb
YFun <omycoke@outlook.com>
2021-02-05 10:25:12 +0800

    Update text.js

--- ads/text.js
+++ ads/text.js
@@ -1,6 +1,8 @@
+function AdsText(tag){
 var html = `
 <div class="ads-text-yfun">
   <a href="https://google.com" rel="nofollow">来自谷歌的广告 — 谷歌搜索</a>
 </div>
 `
-document.getElementByclassname("ads-text-v1").innerHTML=html
+document.queryselectorAll(tag).innerHTML=html
+}
