3312639af4857f84b1c21287ecd052425cc714ad
YFun <omycoke@outlook.com>
2021-02-06 14:25:40 +0800

    Update text-jq.js

--- ads/text-jq.js
+++ ads/text-jq.js
@@ -2,12 +2,16 @@ function AdsText(tag){
 var html = `
 <div class="ads-text-yfun">
   <a href="https://google.com" rel="nofollow" target="_blank">来自谷歌的广告 — 谷歌搜索</a> · 
+<!--
   <a href="https://tools.shengmingyun.top" target="_blank">圣明云收录网</a>
+-->
 </div>
 <div class="ads-img-yfun">
+<!--
   <a href="https://tools.shengmingyun.top" target="_blank">
     <img src="https://7.dusays.com/2021/02/05/058e5a78fe4bd.png" data-src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAERlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAA6ABAAMAAAABAAEAAKACAAQAAAABAAAAAaADAAQAAAABAAAAAQAAAAD5Ip3+AAAADUlEQVQIHWM4c+bMfwAIMANkxSThkAAAAABJRU5ErkJggg==" data-srcset="https://7.dusays.com/2021/02/05/058e5a78fe4bd.png" class="lazyload">
   </a>
+-->
 </div>
 `;
 //let tags = document.querySelector(tag);
